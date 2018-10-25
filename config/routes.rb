5e68722b9e66a42f7d9b41aef90c0c51809a82e4
Andrew Stasson <astass3000@live.com>
2018-10-24 19:37:56 -0500

    5.1 Added routes to point to custom page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   resources :orders, only: [:index, :show, :create, :destroy]
   get 'simple_pages/about'
   get 'simple_pages/contact'
-  root 'simple_pages#landing_page'
+  root 'products#index'
   get 'simple_pages/index'
+  get 'simple_pages/landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
