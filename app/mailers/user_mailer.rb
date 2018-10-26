c0073e1fd91cf700a69e6668237c55fdfb7318ae
Andrew Stasson <astass3000@live.com>
2018-10-26 10:25:11 -0500

    5.3 changed email address

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ApplicationMailer
   def contact_form(email, name, message)
     @message = message
     mail(from: email,
-         to: "your-email@example.com",
-         subject: "Anew contact form message from #{name}")
-  end 
+         to: "astass3000@live.com",
+         subject: "A new contact form message from #{name}")
+  end
 end
