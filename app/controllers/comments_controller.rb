c4474627e7c7e868d1ee9dda0cfb03feef854cb4
Andrew Stasson <astass3000@live.com>
2018-11-12 09:53:58 -0600

    5.9 added validation and pagination to products pages

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,8 +3,16 @@ class CommentsController < ApplicationController
     @product = Product.find(params[:product_id])
     @comment = @product.comments.new(comment_params)
     @comment.user = current_user
-    @comment.save
-    redirect_to product_path(@product)
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @product, notice: 'Review was created successfully' }
+        format.json { render :show, status: :created, location: @product }
+      else
+        format.html { redirect_to @product, alert: 'Review was not saved successfully' }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
@@ -14,5 +22,9 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:user_id, :body, :rating)
-  end 
+  end
+
+  def index
+    @comments = Comment.order('created_at DESC').paginate(:page => params[:page], :per_page => 5)
+  end
 end
