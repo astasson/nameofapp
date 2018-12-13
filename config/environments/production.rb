d76148a86a4b34819206755fc3b35ecdfb0d75d6
Andrew Stasson <astass3000@live.com>
2018-12-12 19:04:57 -0600

    6.13 added real time updates with ActionCable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,4 +103,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'andrewstasson.herokuapp.com' }
+
+  config.web_socket_server_url = "wss://andrewstasson.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://andrewstasson.herokuapp.com', 'http://andrewstasson.herokuapp.com']
 end
