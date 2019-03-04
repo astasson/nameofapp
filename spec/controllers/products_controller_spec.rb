c1a461b3a4ab8d88f26850531672964598ad892c
Andrew Stasson <astass3000@live.com>
2019-03-04 07:52:53 -0600

    6.15 add tests to controller spec

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 describe ProductsController, type: :controller do
   before do
-    @product = {
+    @product = Product.create(
       name: "Fish",
       description: "food",
       image_url: "https://www.gannett-cdn.com/presto/2018/09/10/PDTF/cba7a9ee-2e11-4707-9a46-cc4ec7a1d15d-Pepperoni_Pizza.jpg?width=534&height=401&fit=bounds&auto=webp",
       prices: 500,
-    }
+    )
   end
   describe 'GET #index' do
     it 'displays all products' do
@@ -19,10 +19,14 @@ describe ProductsController, type: :controller do
 
   describe 'GET #show' do
     before do
-      @comment = "meh"
+      @comment = Comment.create(
+        user_id: 1,
+        body: "meh",
+        rating: 3,
+        product_id: @product.id,
+      )
     end
     it 'displays pages of comments' do
-      get :show
 
     end
   end
@@ -37,7 +41,7 @@ describe ProductsController, type: :controller do
 
   describe 'GET #edit' do
     it 'displays product editing form' do
-      get :edit
+      get :edit, params: { id: @product.id }
       expect(response).to be_ok
       expect(response).to render_template('products/edit')
     end
@@ -46,30 +50,31 @@ describe ProductsController, type: :controller do
   describe 'POST #create' do
     context "if product is saved" do
       it 'creates new products' do
-        @product.save
-        expect(response).to render_template('/products')
+        post :create, params: { product: { description: "New product"} }
+        expect(response).to render_template('new')
       end
     end
 
     context "if product is not saved" do
       it "renders new product form" do
-        expect(response).to render_template('/products/new')
+        post :create, params: { product: { description: nil } }
+        expect(response).to render_template('new')
       end
-    end  
+    end
   end
 
   describe 'PATCH #update' do
-
     context "if product is updated" do
       it "renders new product page" do
-        @product.update(name: "Pizza")
-        expect(response).to render_template('/products/1')
+        put :update, params: { id: @product.id, product: { name: "Pizza" } }
+        expect(response).to redirect_to('/products/1')
       end
     end
 
     context "if product is not updated" do
       it "redirects to product edit page" do
-        expect(response).to render_template("/products/1/edit")
+        put :update, params: { id: @product.id, product: { name: nil } }
+        expect(response).to render_template("edit")
       end
     end
 
@@ -77,6 +82,7 @@ describe ProductsController, type: :controller do
 
   describe 'DELETE #destroy' do
     it 'deletes products' do
+      delete :destroy, params: { id: @product.id }
       @product.destroy
       expect(response).to redirect_to "/products"
     end
